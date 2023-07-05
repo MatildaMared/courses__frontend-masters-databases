fdaf765becffeec13c93ec569192eafb1b1ce0d9
Matilda Mared <matildamared@live.se>
2023-07-05 06:27:27 +0200

    add server logic

---
+++ node-redis/server.js
@@ -0,0 +1,28 @@
+const { promisify } = require("util");
+const express = require("express");
+const redis = require("redis");
+const client = redis.createClient();
+
+const rIncr = promisify(client.incr).bind(client);
+
+async function init() {
+	const app = express();
+
+	app.get("/pageview", async (req, res) => {
+		const views = await rIncr("pageviews");
+		res
+			.json({
+				status: "ok",
+				views,
+			})
+			.end();
+	});
+
+	const PORT = 3000;
+	app.use(express.static("./static"));
+	app.listen(PORT, () => {
+		console.log(`Server listening on port ${PORT}... 🤩`);
+	});
+}
+
+init();
